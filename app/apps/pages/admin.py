eafd06a676be0537a07d79c4e3b1a781f3e42e0a
Isaias Suarez <isadetaseek@gmail.com>
2025-04-15 08:15:12 -0400

    agregar: testimonial al models.py agregar: modelos al admin.py

--- app/apps/pages/admin.py
+++ app/apps/pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Suscriptor, Brand, SiteImages, Messages, Testimonial
 
 # Register your models here.
+admin.site.register(Suscriptor)
+admin.site.register(Brand)
+admin.site.register(SiteImages)
+admin.site.register(Messages)
+admin.site.register(Testimonial)
--- app/apps/pages/models.py
+++ app/apps/pages/models.py
@@ -62,3 +62,19 @@ class Messages(models.Model):
         verbose_name = "Mensaje"
         verbose_name_plural = "Mensajes"
         ordering = ["-created_at"]
+
+
+class Testimonial(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="testimonials/", blank=True, null=True)
+    score = models.IntegerField(default=5)  # You can set a default score
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Testimonio"
+        verbose_name_plural = "Testimonios"
+        ordering = ["-created_at"]
