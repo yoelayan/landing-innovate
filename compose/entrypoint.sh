d17a178ec53e2919bb277cbdc590c746ab1fe999
yoelayan <yoeldategeek@gmail.com>
2025-05-10 00:35:17 +0100

    fix: update entrypoint script to correctly wait for PostgreSQL service using the host address

--- compose/entrypoint.sh
+++ compose/entrypoint.sh
@@ -10,7 +10,7 @@ if [ -z "${DATABASE_USER}" ]; then
 fi
 export DATABASE_URL="postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}"
 
-wait-for-it "${DATABASE_NAME}:${DATABASE_PORT}" -t 30
+wait-for-it ${DATABASE_HOST}:${DATABASE_PORT} -t 30
 
 >&2 echo 'PostgreSQL is available'
 
