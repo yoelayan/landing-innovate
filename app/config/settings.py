8c83333c651803eaee3371d1b329c745b5490a89
yoelayan <yoeldategeek@gmail.com>
2025-05-10 00:12:39 +0100

    feat: switch database configuration from SQLite to PostgreSQL; utilize environment variables for database settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -96,11 +96,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# Default to SQLite
+# Default PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
